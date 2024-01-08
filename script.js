a458af4c4306b71e247c21454d8a9056beb336cc
Bhupesh Dahiya <149587292+bhupesh56@users.noreply.github.com>
2024-01-08 13:26:12 +0530

    make script to play 5 games

--- script.js
+++ script.js
@@ -16,23 +16,66 @@ function randomNumber(min=1, max=3) {
     userChoice === "paper" && computerChoice === "scissor" ||
     userChoice === "scissor" && computerChoice === "rock")
     {
-        return(`You Won, ${userChoice} beats ${computerChoice}`);
+        console.log(`You Won, ${userChoice} beats ${computerChoice}\n`);
+        return "W";
     }
     else if (computerChoice === "rock" && userChoice === "paper" || 
     computerChoice === "paper" && userChoice === "scissor" ||
     computerChoice === "scissor" && userChoice=== "rock")
     {
-        return(`You Lose, ${computerChoice} beats ${userChoice}`);
+        console.log(`You Lose, ${computerChoice} beats ${userChoice}\n`);
+        return "L";
     }
     else if (userChoice === computerChoice){
-        return( "It's a draw. Do a rematch \n");
+        console.log( "It's a draw. Do a rematch \n");
+        return "D";
     }
     else{
-        return("enter a valid choice");
+        console.log("enter a valid choice\n");
+        return "I";
     }
   }
 
-  let userChoice = prompt("Enter a Choice:");
-  let computerChoice = getComputerChoice();
+function game(number = 5){
+    let userWin = 0;
+    let computerWin = 0;
 
-  console.log(playRound(userChoice, computerChoice));
+    for (let i = 0; i < number; i++){
+        let userChoice = prompt("Enter a Choice:");
+        let computerChoice = getComputerChoice();
+        let roundResult = playRound(userChoice, computerChoice);
+        if (roundResult === 'W'){
+            userWin++;
+        }
+        else if (roundResult === 'L'){
+            computerWin++;
+        }
+        else{
+            number++;
+        }
+        if(number === 20){
+            break;
+        }
+    }
+
+    if (number === 20){
+        console.log("Too many tries\n");
+    }
+    result(userWin, computerWin);
+}
+
+function result(userWin, computerWin){
+    console.log("\n");
+    if (userWin > computerWin){
+        console.log("You Won\n");
+    }
+    else if(userWin < computerWin){
+        console.log("You Lose");
+    }
+    else{
+        console.log("It's a draw");
+    }
+    console.log(`Your score: ${userWin}\nComputer score: ${computerWin}`);
+}
+
+game();
